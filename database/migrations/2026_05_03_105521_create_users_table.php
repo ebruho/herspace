82c9d51bc84242f2cfb5c88af96e386662450186
ebruh <ebruosman68@gmail.com>
2026-05-10 17:13:09 +0300

    creating tables - user, roles, cities and countries

--- database/migrations/2026_05_03_105521_create_users_table.php
+++ database/migrations/2026_05_03_105521_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('bio')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->foreignId('city_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('role_id')->default(1)->constrained()->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        
+        $this->call([
+            RolesSeeder::class,
+        ]);
+
+        $roleId = DB::table('roles')->where('name', 'user')->value('id');
+
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role_id' => $roleId,
         ]);
+
+
     }
 }
